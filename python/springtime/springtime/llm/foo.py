e78ccf5ecfdcbfc2805d8130b1d2aaeca337ecea
Nasr Maswood <nasr@songbirdcare.com>
2023-04-28 14:18:14 -0400

    begin llm work

---
+++ python/springtime/springtime/llm/foo.py
@@ -0,0 +1,2 @@
+def add1(n: int) -> int:
+    return n + 1
--- python/springtime/springtime/tests/test_langchain.py
+++ python/springtime/springtime/tests/test_langchain.py
@@ -1,6 +1,5 @@
-def func(x):
-    return x + 1
+from springtime.llm.foo import add1
 
 
 def test_answer():
-    assert func(3) == 5
+    assert add1(4) == 5
