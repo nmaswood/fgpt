1760b829643db132b7582345c31e1d7fc1478e1e
Nasr Maswood <nasr@songbirdcare.com>
2023-04-29 23:11:22 -0400

    foo bar

--- typescript/packages/app/pages/index.tsx
+++ typescript/packages/app/pages/index.tsx
@@ -1,4 +1,10 @@
-import { Autocomplete, Box, TextField, LinearProgress } from "@mui/material";
+import {
+  Autocomplete,
+  Box,
+  LinearProgress,
+  TextField,
+  Typography,
+} from "@mui/material";
 import * as React from "react";
 
 import { useFetchDataForTicker } from "../src/hooks/use-fetch-data-for-ticker";
@@ -11,28 +17,52 @@ const Home: React.FC = () => {
   console.log({ questions });
 
   return (
-    <Box padding={3}>
-      <Autocomplete
-        value={ticker ?? null}
-        onChange={(_, newValue) => {
-          setTicker(newValue ?? undefined);
-        }}
-        options={data ?? []}
-        sx={{ width: 300 }}
-        renderInput={({ size, ...rest }) => (
-          // eslint-disable-next-line
-          // @ts-ignore
-          <TextField
-            variant="outlined"
-            label="Select Ticker"
-            size={size}
-            {...rest}
+    <Box padding={3} gap={3}>
+      <Typography align="center" variant="h3">
+        💰 Finance GPT
+      </Typography>
+
+      <Box display="flex" paddingY={1}>
+        <Autocomplete
+          value={ticker ?? null}
+          onChange={(_, newValue) => {
+            setTicker(newValue ?? undefined);
+          }}
+          options={data ?? []}
+          sx={{ width: 300 }}
+          renderInput={({ size, ...rest }) => (
+            // eslint-disable-next-line
+            // @ts-ignore
+            <TextField
+              variant="outlined"
+              label="Select Ticker"
+              size={size}
+              {...rest}
+            />
+          )}
+        />
+      </Box>
+      <Box display="flex" width="100%">
+        {isLoading && (
+          <LinearProgress
+            sx={{
+              width: "100%",
+            }}
           />
         )}
-      />
-      <Box display="flex" width="100%">
-        {isLoading && <LinearProgress />}
       </Box>
+      {questions.length > 0 && (
+        <Box display="flex" flexDirection="column">
+          <Typography>Questions to consider:</Typography>
+          <Box display="flex" flexDirection="column">
+            {questions.map((question) => (
+              <Typography key={question} variant="body1">
+                {question}
+              </Typography>
+            ))}
+          </Box>
+        </Box>
+      )}
     </Box>
   );
 };
