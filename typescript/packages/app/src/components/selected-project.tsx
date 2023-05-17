42e3d9bd7e4e5b874edb018207669a2a4466e379
Nasr Maswood <nasr@songbirdcare.com>
2023-05-17 17:42:41 -0400

    foo

--- typescript/packages/app/src/components/selected-project.tsx
+++ typescript/packages/app/src/components/selected-project.tsx
@@ -30,7 +30,7 @@ export const SelectedProject: React.FC<{ project: Project }> = ({
 
   const [value, setValue] = React.useState(0);
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+  const handleChange = (_: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
 
