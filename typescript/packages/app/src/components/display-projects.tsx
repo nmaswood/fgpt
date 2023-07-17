57efd77a098a80f0bbe655a828775a492d6ed212
Nasr Maswood <nasr@songbirdcare.com>
2023-07-17 14:36:00 -0400

    add useHover hook

--- typescript/packages/app/src/components/display-projects.tsx
+++ typescript/packages/app/src/components/display-projects.tsx
@@ -16,7 +16,6 @@ import {
   MenuItem,
   Typography,
 } from "@mui/joy";
-import { useHover } from "@uidotdev/usehooks";
 import React from "react";
 
 export const DisplayProjects: React.FC<{
@@ -265,3 +264,32 @@ const ProjectCard: React.FC<{
     </Badge>
   );
 };
+
+type UseHoverType<T extends HTMLElement> = [React.RefObject<T>, boolean];
+
+function useHover<T extends HTMLElement>(): UseHoverType<T> {
+  const [value, setValue] = React.useState(false);
+
+  const ref = React.useRef<T>(null);
+
+  const handleMouseOver = () => setValue(true);
+  const handleMouseOut = () => setValue(false);
+
+  React.useEffect(
+    () => {
+      const node = ref.current;
+      if (node) {
+        node.addEventListener("mouseover", handleMouseOver);
+        node.addEventListener("mouseout", handleMouseOut);
+
+        return () => {
+          node.removeEventListener("mouseover", handleMouseOver);
+          node.removeEventListener("mouseout", handleMouseOut);
+        };
+      }
+    },
+    [ref.current], // Recall only if ref changes
+  );
+
+  return [ref, value];
+}
