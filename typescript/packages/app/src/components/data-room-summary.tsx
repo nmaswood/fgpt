d2487d0bc16b0d3c23c2fb3c7c4ad74fb2228dcb
Nasr Maswood <nasr@songbirdcare.com>
2023-07-19 17:19:02 -0400

    backload file status

--- typescript/packages/app/src/components/data-room-summary.tsx
+++ typescript/packages/app/src/components/data-room-summary.tsx
@@ -1,6 +1,14 @@
 import { assertNever, RenderShowCaseFile } from "@fgpt/precedent-iso";
 import { Term } from "@fgpt/precedent-iso/src/models/llm-outputs";
-import { Box, Button, CircularProgress, Table,Typography } from "@mui/joy";
+import PictureAsPdfOutlinedIcon from "@mui/icons-material/PictureAsPdfOutlined";
+import {
+  Alert,
+  Box,
+  Button,
+  CircularProgress,
+  Table,
+  Typography,
+} from "@mui/joy";
 import Image from "next/image";
 import React from "react";
 
@@ -26,7 +34,9 @@ export const DataRoomSummary: React.FC<{
     >
       <Box display="flex" width="100%" justifyContent="space-between">
         <Typography level="h4">Data room summary</Typography>
-        <Button disabled={true}>View full report</Button>
+        {showCaseFile && showCaseFile.type == "set" && (
+          <Button disabled={true}>View full report</Button>
+        )}
       </Box>
 
       {loading && (
@@ -61,11 +71,20 @@ const Dispatch: React.FC<{ showCaseFile: RenderShowCaseFile.File }> = ({
 }) => {
   switch (showCaseFile.type) {
     case "not_set":
-      return null;
+      return (
+        <Box
+          display="flex"
+          width="100%"
+          height="100%"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Alert size="lg" variant={"outlined"} color="info">
+            A CIM is currently not selected for this project.{" "}
+          </Alert>
+        </Box>
+      );
     case "set":
-      if (!showCaseFile.url) {
-        return null;
-      }
       return (
         <Box
           display="flex"
@@ -74,17 +93,77 @@ const Dispatch: React.FC<{ showCaseFile: RenderShowCaseFile.File }> = ({
           maxHeight="100%"
           maxWidth="100%"
           overflow="auto"
+          gap={3}
         >
-          <Image
-            placeholder="blur"
-            blurDataURL={BLUR_DATA_URL}
-            src={showCaseFile.url}
-            width={500}
-            height={500}
-            alt="thumbnail of CIM"
-          />
-          <Box display="flex" width="100%" height="100%">
-            <TermsTable terms={showCaseFile.terms} />
+          {!showCaseFile.url && (
+            <Box
+              display="flex"
+              overflow="auto"
+              gap={3}
+              width="320px"
+              height="407px"
+            >
+              <Box
+                width="100%"
+                height="100%"
+                display="flex"
+                borderRadius={8}
+                border="1px solid #E5E5E5"
+                boxShadow="rgba(0, 0, 0, 0.06) 0px 2px 4px"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <PictureAsPdfOutlinedIcon
+                  color="error"
+                  sx={{
+                    fontSize: 36,
+                    color: "danger.solidColor",
+                  }}
+                />
+              </Box>
+            </Box>
+          )}
+          {showCaseFile.url && (
+            <Box
+              display="flex"
+              borderRadius={8}
+              border="1px solid #E5E5E5"
+              boxShadow="rgba(0, 0, 0, 0.06) 0px 2px 4px"
+            >
+              <Image
+                placeholder="blur"
+                blurDataURL={BLUR_DATA_URL}
+                src={showCaseFile.url}
+                width={320}
+                height={407}
+                alt="thumbnail of CIM"
+              />
+            </Box>
+          )}
+          <Box
+            display="flex"
+            width="100%"
+            height="100%"
+            maxHeight="100%"
+            maxWidth="100%"
+            overflow="auto"
+          >
+            {showCaseFile.terms.length === 0 && (
+              <Alert
+                size="lg"
+                variant={"outlined"}
+                color="info"
+                sx={{
+                  width: "100%",
+                  justifyContent: "center",
+                }}
+              >
+                No terms have been extracted from the document
+              </Alert>
+            )}
+            {showCaseFile.terms.length > 0 && (
+              <TermsTable terms={showCaseFile.terms} />
+            )}
           </Box>
         </Box>
       );
@@ -100,29 +179,56 @@ const TermsTable: React.FC<{
     <Table
       variant="outlined"
       sx={{
+        display: "inline-block",
         borderRadius: 8,
         border: "1px solid #E5E5E5",
         background: "#FFF",
       }}
     >
-      <thead>
-        <tr>
-          <th>
-            <Typography>Term</Typography>
-          </th>
-          <th>
-            <Typography>Value</Typography>
-          </th>
-        </tr>
-      </thead>
+      <colgroup>
+        <col
+          style={{
+            width: "35%",
+          }}
+        />
+        <col
+          style={{
+            width: "65%",
+          }}
+        />
+      </colgroup>
+
       <tbody>
         {terms.map((term, index) => (
-          <tr key={index}>
-            <td scope="row">
-              <Typography>{term.termName}</Typography>
+          <tr
+            key={index}
+            style={{
+              padding: "8px",
+              height: "40px",
+            }}
+          >
+            <td
+              scope="row"
+              style={{
+                padding: "8px",
+              }}
+            >
+              <Typography
+                fontWeight={700}
+                sx={{
+                  padding: 1,
+                  color: "#666",
+                }}
+              >
+                {term.termName}
+              </Typography>
             </td>
-            <td>
-              <Typography>{term.termValue}</Typography>
+            <td
+              style={{
+                padding: "8px",
+              }}
+            >
+              <Typography fontWeight={400}>{term.termValue}</Typography>
             </td>
           </tr>
         ))}
