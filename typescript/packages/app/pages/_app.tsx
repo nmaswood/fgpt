e398e4fcc25564098f3bbb14db8a2597c47666d7
Nasr Maswood <nasr@songbirdcare.com>
2023-07-24 15:48:40 -0400

    try again

--- typescript/packages/app/pages/_app.tsx
+++ typescript/packages/app/pages/_app.tsx
@@ -23,7 +23,11 @@ export default function App({ Component, pageProps }: AppProps) {
           content="AI-First Tools for Modern Financial Analysis"
         />
         <link rel="icon" href="/favicon.png" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <meta
+          name="viewport"
+          content="initial-scale=1, width=device-width, user-scalable=yes"
+        />
       </Head>
       <MaterialCssVarsProvider theme={{ [MATERIAL_THEME_ID]: THEME }}>
         <JoyCssVarsProvider theme={JOY_THEME}>
